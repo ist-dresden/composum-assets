b42b089459dc5f7210c897cd6cb8d9e6a59b07e0
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-10-08 14:27:13 +0200

    update existing nodetypes in SetupHook

--- commons/setup/src/main/java/com/composum/assets/commons/setup/SetupHook.java
+++ commons/setup/src/main/java/com/composum/assets/commons/setup/SetupHook.java
@@ -4,9 +4,12 @@ import com.composum.sling.core.usermanagement.core.UserManagementService;
 import org.apache.jackrabbit.api.JackrabbitSession;
 import org.apache.jackrabbit.api.security.user.Authorizable;
 import org.apache.jackrabbit.api.security.user.UserManager;
+import org.apache.jackrabbit.commons.cnd.CndImporter;
+import org.apache.jackrabbit.vault.fs.io.Archive;
 import org.apache.jackrabbit.vault.packaging.InstallContext;
 import org.apache.jackrabbit.vault.packaging.InstallHook;
 import org.apache.jackrabbit.vault.packaging.PackageException;
+import org.apache.jackrabbit.vault.packaging.VaultPackage;
 import org.apache.jackrabbit.vault.util.JcrConstants;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
@@ -18,9 +21,15 @@ import org.slf4j.LoggerFactory;
 import javax.jcr.Node;
 import javax.jcr.NodeIterator;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.nodetype.NodeType;
+import javax.jcr.nodetype.NodeTypeManager;
 import javax.jcr.query.Query;
 import javax.jcr.query.QueryManager;
 import javax.jcr.query.QueryResult;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -40,7 +49,7 @@ public class SetupHook implements InstallHook {
 
     static {
         ASSETS_SYSTEM_USERS = new LinkedHashMap<>();
-        ASSETS_SYSTEM_USERS.put(ASSETS_SYSTEM_USERS_PATH + ASSETS_SERVICE_USER, Collections.<String>emptyList());
+        ASSETS_SYSTEM_USERS.put(ASSETS_SYSTEM_USERS_PATH + ASSETS_SERVICE_USER, Collections.emptyList());
     }
 
     public static final Map<String, List<String>> ASSETS_GROUPS;
@@ -56,6 +65,8 @@ public class SetupHook implements InstallHook {
             case INSTALLED:
                 LOG.info("installed: execute...");
                 setupGroupsAndUsers(ctx);
+                // updateNodeTypes should be the last actions since we need a session.save() there.
+                updateNodeTypes(ctx);
                 LOG.info("installed: execute ends.");
         }
     }
@@ -82,7 +93,7 @@ public class SetupHook implements InstallHook {
                 }
             }
             session.save();
-            addVersionableMixinToAssetContentNodes(ctx, session);
+            addVersionableMixinToAssetContentNodes(session);
         } catch (RepositoryException | RuntimeException rex) {
             LOG.error(rex.getMessage(), rex);
             throw new PackageException(rex);
@@ -93,7 +104,7 @@ public class SetupHook implements InstallHook {
      * We want all jcr:content nodes containing an cpa:AssetConfiguration to be versionable so that they can be
      * published. Thus, we need to add a mix:versionable if need be.
      */
-    protected void addVersionableMixinToAssetContentNodes(InstallContext ctx, JackrabbitSession session) throws RepositoryException {
+    protected void addVersionableMixinToAssetContentNodes(JackrabbitSession session) throws RepositoryException {
         QueryManager queryManager = session.getWorkspace().getQueryManager();
         Query query = queryManager.createQuery("select * from [cpa:AssetConfiguration]", Query.JCR_SQL2);
         QueryResult result = query.execute();
@@ -102,11 +113,38 @@ public class SetupHook implements InstallHook {
             if (node.isNodeType("cpa:AssetConfiguration")) {
                 while (node != null && !node.getName().equals(JcrConstants.JCR_CONTENT)) { node = node.getParent(); }
                 if (node != null && !node.isNodeType(JcrConstants.MIX_VERSIONABLE)) {
+                    LOG.info("Adding {} to {}", JcrConstants.MIX_VERSIONABLE, node.getPath());
                     node.addMixin(JcrConstants.MIX_VERSIONABLE);
                 }
             }
         }
     }
+
+    protected void updateNodeTypes(InstallContext ctx) throws PackageException {
+        try {
+            Session session = ctx.getSession();
+            NodeTypeManager nodeTypeManager = session.getWorkspace().getNodeTypeManager();
+            NodeType assetContentType = nodeTypeManager.getNodeType("cpa:AssetContent");
+            if (!assetContentType.isNodeType(org.apache.jackrabbit.JcrConstants.MIX_VERSIONABLE)) {
+                LOG.info("Updating nodetypes - cpa:AssetContent is not yet versionable.");
+                try (VaultPackage vaultPackage = ctx.getPackage()) {
+                    Archive archive = vaultPackage.getArchive();
+                    try (InputStream stream = archive.openInputStream(archive.getEntry("/META-INF/vault/nodetypes.cnd"))) {
+                        if (stream != null) {
+                            InputStreamReader cndReader = new InputStreamReader(stream, StandardCharsets.UTF_8);
+                            CndImporter.registerNodeTypes(cndReader, session, true);
+                        }
+                    }
+                }
+            } else {
+                LOG.info("No nodetype update needed - cpa:AssetContent is already versionable.");
+            }
+        } catch (Exception rex) {
+            LOG.error(rex.getMessage(), rex);
+            throw new PackageException(rex);
+        }
+    }
+
 
     @SuppressWarnings("unchecked")
     public static <T> T getService(Class<T> type) {
