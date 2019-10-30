ce5254d22d18a7fbd79273a0fd024dd13e5d442c
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-10-30 13:44:08 +0100

    test together with staging to weed out replication problems

--- commons/bundle/src/test/java/com/composum/assets/commons/service/AdaptiveImageServiceTest.java
+++ commons/bundle/src/test/java/com/composum/assets/commons/service/AdaptiveImageServiceTest.java
@@ -8,34 +8,49 @@ import com.composum.sling.core.BeanContext;
 import com.composum.sling.core.concurrent.LazyCreationServiceImpl;
 import com.composum.sling.core.concurrent.SemaphoreSequencer;
 import com.composum.sling.core.util.ResourceUtil;
+import com.composum.sling.platform.staging.ReleaseChangeEventPublisher;
+import com.composum.sling.platform.staging.ReleasedVersionable;
+import com.composum.sling.platform.staging.StagingConstants;
+import com.composum.sling.platform.staging.StagingReleaseManager;
+import com.composum.sling.platform.staging.impl.DefaultStagingReleaseManager;
+import com.composum.sling.platform.staging.impl.ReleaseChangeEventPublisherImpl;
+import com.composum.sling.platform.staging.query.QueryBuilder;
+import com.composum.sling.platform.staging.query.impl.QueryBuilderAdapterFactory;
+import com.composum.sling.platform.testing.testutil.AnnotationWithDefaults;
 import com.composum.sling.platform.testing.testutil.ErrorCollectorAlwaysPrintingFailures;
 import com.composum.sling.platform.testing.testutil.JcrTestUtils;
-import com.google.common.collect.ImmutableMap;
+import com.google.common.base.Function;
 import org.apache.commons.io.IOUtils;
 import org.apache.jackrabbit.commons.cnd.CndImporter;
 import org.apache.sling.api.resource.ModifiableValueMap;
 import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.testing.mock.sling.ResourceResolverType;
 import org.apache.sling.testing.mock.sling.junit.SlingContext;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import javax.jcr.Node;
 import javax.jcr.Session;
 import javax.jcr.nodetype.NodeType;
 import javax.jcr.version.Version;
 import javax.jcr.version.VersionManager;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Tests the creation of renditions.
@@ -48,9 +63,11 @@ public class AdaptiveImageServiceTest {
     private AssetsService assetService;
     private BeanContext beanContext;
     private Resource asset1Resource;
+    private @NotNull ResourceResolver resolver;
 
     @Rule
     public final ErrorCollectorAlwaysPrintingFailures ec = new ErrorCollectorAlwaysPrintingFailures();
+    private ResourceResolver resourceResolver;
 
     /**
      * Uses content copied from the prototype site,
@@ -58,12 +75,13 @@ public class AdaptiveImageServiceTest {
      */
     @Before
     public void setup() throws Exception {
-        Session session = context.resourceResolver().adaptTo(Session.class);
+        resolver = context.resourceResolver();
+        Session session = resolver.adaptTo(Session.class);
         InputStreamReader cndReader = new InputStreamReader(getClass().getResourceAsStream("/adaptiveImageServiceTest" +
                 "/nodetypes.cnd"));
         NodeType[] nodeTypes = CndImporter.registerNodeTypes(cndReader, session);
         assertEquals(10, nodeTypes.length);
-        context.resourceResolver().commit();
+        resolver.commit();
         context.build().resource("/test/assets").commit();
         context.load().json("/adaptiveImageServiceTest/site-1.json", "/test/assets/site-1");
         context.load().binaryFile("/adaptiveImageServiceTest/images/image-1.png/wide/original/image-1.png",
@@ -76,14 +94,14 @@ public class AdaptiveImageServiceTest {
         service = context.registerInjectActivateService(new DefaultAdaptiveImageService());
         assetService = context.registerInjectActivateService(new DefaultAssetsService());
 
-        beanContext = new BeanContext.Service(context.resourceResolver());
-        asset1Resource = context.resourceResolver().getResource("/test/assets/site-1/images/image-1.png");
+        beanContext = new BeanContext.Service(resolver);
+        asset1Resource = resolver.getResource("/test/assets/site-1/images/image-1.png");
     }
 
     @After
     public void tearDown() throws PersistenceException {
         // make sure everything is in a consistent state.
-        context.resourceResolver().commit();
+        resolver.commit();
     }
 
     @Test
@@ -103,7 +121,7 @@ public class AdaptiveImageServiceTest {
         assertTrue(asset.isValid());
         ec.checkThat(asset.getTransientsPath().replaceAll("workspace-[0-9]*", "workspace-time"),
                 is("/var/composum/assets/test/assets/site-1/theuuidofthereplicatedversion/images/image-1.png/workspace-time"));
-        ec.onFailure(() -> JcrTestUtils.printResourceRecursivelyAsJson(context.resourceResolver(),
+        ec.onFailure(() -> JcrTestUtils.printResourceRecursivelyAsJson(resolver,
                 "/test/assets/site-1/images/image-1.png/thumbnail"));
 
         AssetRendition rendition = service.getOrCreateRendition(asset, "thumbnail", "medium");
@@ -159,8 +177,8 @@ public class AdaptiveImageServiceTest {
         assertNotNull(stream);
         Resource assetResource = assetService.createImageAsset(beanContext, "/test/assets/site-1/images",
                 "newasset.png", "bar", stream);
-        ec.onFailure(() -> JcrTestUtils.printResourceRecursivelyAsJson(context.resourceResolver(), assetResource.getPath()));
-        ec.onFailure(() -> JcrTestUtils.printResourceRecursivelyAsJson(context.resourceResolver(), AssetsConstants.PATH_TRANSIENTS));
+        ec.onFailure(() -> JcrTestUtils.printResourceRecursivelyAsJson(resolver, assetResource.getPath()));
+        ec.onFailure(() -> JcrTestUtils.printResourceRecursivelyAsJson(resolver, AssetsConstants.PATH_TRANSIENTS));
         // the meta node is specified as autocreated, but Jackrabbit doesn't create it for unknown reasons in this
         // test. :-( So we have to do it manually to be able to commit.
         // FIXME(hps,25.10.19) what about autocreated meta? Autocreating it doesn't work right now as it is
@@ -168,7 +186,7 @@ public class AdaptiveImageServiceTest {
         // import.
         context.build().resource("test/assets/site-1/images/newasset.png/jcr:content/meta",
                 ResourceUtil.PROP_PRIMARY_TYPE, AssetsConstants.NODE_TYPE_META_DATA);
-        context.resourceResolver().commit();
+        resolver.commit();
 
         ImageAsset asset = new ImageAsset(beanContext, assetResource);
         AssetRendition rendition = service.getOrCreateRendition(asset, "bar", "medium");
@@ -191,16 +209,51 @@ public class AdaptiveImageServiceTest {
     @Test
     public void checkinAsset() throws Exception {
         String path = "/test/assets/site-1/images/image-1.png/wide/original/image-1.png/jcr:content";
-        Resource resource = context.resourceResolver().getResource(path);
-        context.resourceResolver().create(resource, "meta", ImmutableMap.of(ResourceUtil.PROP_PRIMARY_TYPE,
-                AssetsConstants.NODE_TYPE_META_DATA));
+        Resource resource = resolver.getResource(path);
+        // autocreated:
+        // resourceResolver.create(resource, "meta", ImmutableMap.of(ResourceUtil.PROP_PRIMARY_TYPE,
+        // AssetsConstants.NODE_TYPE_META_DATA));
         resource.adaptTo(ModifiableValueMap.class).put(ResourceUtil.PROP_MIXINTYPES,
                 new String[]{AssetsConstants.MIXIN_TYPE_ASSET_RESOURCE});
-        context.resourceResolver().commit();
-        JcrTestUtils.printResourceRecursivelyAsJson(context.resourceResolver(), path);
-        VersionManager versionManager = context.resourceResolver().adaptTo(Session.class).getWorkspace().getVersionManager();
+        resolver.commit();
+        // JcrTestUtils.printResourceRecursivelyAsJson(resourceResolver, path);
+        VersionManager versionManager = resolver.adaptTo(Session.class).getWorkspace().getVersionManager();
         Version version = versionManager.checkin(path);
         assertNotNull(version);
+
+        // try behavior in staging.
+        // Staging setup from DefaultStagingReleaseManagerTest:
+        InputStreamReader cndReader = new InputStreamReader(getClass().getResourceAsStream("/adaptiveImageServiceTest" +
+                "/stagingNodetypes.cnd"));
+        NodeType[] nodeTypes = CndImporter.registerNodeTypes(cndReader, resolver.adaptTo(Session.class));
+        assertEquals(2, nodeTypes.length);
+
+        DefaultStagingReleaseManager releaseManager = new DefaultStagingReleaseManager() {{
+            this.configuration = AnnotationWithDefaults.of(DefaultStagingReleaseManager.Configuration.class);
+            this.publisher = new ReleaseChangeEventPublisherImpl();
+            this.resolverFactory = mock(ResourceResolverFactory.class);
+            when(this.resolverFactory.getServiceResourceResolver(null)).thenAnswer((x) -> context.resourceResolver().clone(null));
+        }};
+        context.registerAdapter(ResourceResolver.class, QueryBuilder.class,
+                (Function<ResourceResolver, QueryBuilder>) (resolver) ->
+                        new QueryBuilderAdapterFactory().getAdapter(resolver, QueryBuilder.class));
+
+        // Now the test:
+        Resource siteResource = resolver.getResource("/test/assets/site-1");
+        siteResource.adaptTo(ModifiableValueMap.class).put(ResourceUtil.PROP_MIXINTYPES, new String[]{StagingConstants.TYPE_MIX_RELEASE_ROOT});
+        resolver.commit();
+
+        StagingReleaseManager.Release currentRelease = releaseManager.findRelease(siteResource, StagingConstants.CURRENT_RELEASE);
+        releaseManager.updateRelease(currentRelease, Arrays.asList(ReleasedVersionable.forBaseVersion(resource)));
+
+        ResourceResolver stagingResolver = releaseManager.getResolverForRelease(currentRelease, null, false);
+        Resource stagedResource = stagingResolver.resolve(path);
+
+        // JcrTestUtils.printResourceRecursivelyAsJson(stagedResource);
+        ec.checkThat(stagedResource.getValueMap().get(StagingConstants.PROP_REPLICATED_VERSION, String.class), is(version.getIdentifier()));
+        ec.checkThat(stagedResource.adaptTo(Node.class).getProperty(StagingConstants.PROP_REPLICATED_VERSION).getString(),
+                is(version.getIdentifier()));
+
     }
 
 }
