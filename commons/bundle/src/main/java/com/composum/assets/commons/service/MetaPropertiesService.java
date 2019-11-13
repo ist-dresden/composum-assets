23803578154fe239bcd4080f34c827cf596e3a4e
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-11-13 12:12:37 +0100

    ++javadoc

--- commons/bundle/src/main/java/com/composum/assets/commons/service/MetaPropertiesService.java
+++ commons/bundle/src/main/java/com/composum/assets/commons/service/MetaPropertiesService.java
@@ -8,7 +8,22 @@ package com.composum.assets.commons.service;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 
+import javax.annotation.Nonnull;
+
+/**
+ * Service that deals with the metadata for images / videos which are automatically saved on
+ * {@value com.composum.assets.commons.AssetsConstants#PATH_META}.*
+ */
 public interface MetaPropertiesService {
 
-    boolean adjustMetaProperties(ResourceResolver resolver, Resource resource);
+    /**
+     * Update the {@value com.composum.assets.commons.AssetsConstants#PATH_META} node of all subresources of
+     * {resource}.
+     *
+     * @param resolver a resource resolver to write data with
+     * @param resource the resource to look at - we look for files at {resource} and in subfolders.
+     * @return true if the service found a strategy to deal with the given resource.
+     */
+    boolean adjustMetaProperties(@Nonnull ResourceResolver resolver, @Nonnull Resource resource);
+
 }
