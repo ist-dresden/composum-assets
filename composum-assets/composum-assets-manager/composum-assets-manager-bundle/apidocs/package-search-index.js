9bb15702ddb2b3ec3bf018285bac9a992a781b7a
Ralf Wunsch <r.wunsch@ist-software.com>
2023-03-23 10:25:50 +0000

    Deploying to gh-pages from @ ist-dresden/composum-assets@b8b645414857e60ee7c545b07157e455acc9d023 🚀

---
+++ composum-assets/composum-assets-manager/composum-assets-manager-bundle/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.composum.assets.manager"},{"l":"com.composum.assets.manager.image"},{"l":"com.composum.assets.manager.servlet"},{"l":"com.composum.assets.manager.view"}]
